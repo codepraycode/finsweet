8525982f52201c94c8daab497119caa0ca8e4c37
Precious Olusola <preciousolusola16@gmail.com>
2024-04-09 10:07:40 +0100

    users can now download book if they've paid

--- src/components/DownloadBook.tsx
+++ src/components/DownloadBook.tsx
@@ -2,33 +2,76 @@
 
 import { PaystackButton } from 'react-paystack';
 import Input from "./UI/Input";
-import { useState } from "react";
-import { PaymentModel } from '@/lib/nobox/structures';
+import { useEffect, useState } from "react";
+import { Payment, PaymentModel } from '@/lib/nobox/structures';
 import PayStackButton, { PayStackButtonProps } from './PayStackButton';
 import Button from './UI/Button';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 
-const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+async function validateToken(token: string) {
+    const data = await PaymentModel.findOne({reference: token});
+
+    if (!data || data?.used) return null;
+
+    return data;
+}
+
+async function inValidateToken(token: string) {
+    const data = await PaymentModel.findOne({reference: token});
+
+    if (data.used) return null;
+
+    await PaymentModel.updateOne({reference: token}, {used: true});
+}
 
 export default function DownloadBook(props: PayStackButtonProps) {
 
+    const [paymentData, setPaymentData] = useState<Payment & {id: string} | null | undefined>(undefined);
+
+    const searchParams = useSearchParams();
+    const action = searchParams.get("action");
+    const token = searchParams.get("token")
+
+
     
-    const valid = true;
 
-    let template = <PayStackButton {...props}/>;
 
-    if (valid) {
+    useEffect(()=>{
+        (async()=>{
+            if (action === "download" && token) {
+                const payRecord = await validateToken(token);
+
+                // if (!payRecord) return;
+
+                setPaymentData(payRecord);
+            }
+        })()
+    }, [action, token])
+
+    let template;
+    
+    
+    if (paymentData === undefined) template = <p>Loading....</p>;
+
+    if (!paymentData || paymentData.used) template = <PayStackButton {...props}/>;
+
+
+    else {
         template = (
             <>
-                <Link
-                    href={"/"}
+                <a
+                    href={"http://localhost:3000/book.pdf"}
                     download
-                    onClick={(e)=>e.preventDefault()}
+                    onClick={(e)=>{
+                        inValidateToken(paymentData.reference)
+                    }}
                     className='btn btn-download'
                 >
                     Download Book
-                </Link>
+                </a>
             </>
         )
     }
